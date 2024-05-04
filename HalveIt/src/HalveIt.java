7572b9556be01b623191efd103a12364db5d3baf
sangeetagorle <sangeethagorle2@gmail.com>
2024-05-04 14:14:05 +0530

    c

---
+++ HalveIt/src/HalveIt.java
@@ -0,0 +1,9 @@
+
+public class HalveIt {
+	public static double halveTheNumber(double num) {
+		double res=num/2;
+		System.out.println(res);
+		return res;
+	}
+
+}
---
+++ HalveIt/src/HalveItApp.java
@@ -0,0 +1,10 @@
+
+public class HalveItApp {
+
+	public static void main(String[] args) {
+		HalveIt halveit=new HalveIt();
+		HalveIt.halveTheNumber(150.00);
+
+	}
+
+}
