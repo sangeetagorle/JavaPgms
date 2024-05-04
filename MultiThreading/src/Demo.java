b6d91cad7629474932473c921676777c4eec444e
sangeetagorle <sangeethagorle2@gmail.com>
2024-05-04 14:40:12 +0530

    c

---
+++ MultiThreading/src/Demo.java
@@ -0,0 +1,11 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Thread t1=Thread.currentThread();
+		System.out.println("current thread="+t1);
+		// TODO Auto-generated method stub
+
+	}
+
+}
