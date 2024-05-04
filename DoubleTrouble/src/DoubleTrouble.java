96f73afae81b6844b1e86934f7d3acbad85968cf
sangeetagorle <sangeethagorle2@gmail.com>
2024-05-04 13:24:26 +0530

    c

---
+++ DoubleTrouble/src/DoubleTrouble.java
@@ -0,0 +1,15 @@
+
+public class DoubleTrouble {
+
+	public static void main(String[] args)
+	{
+		int num=doubleTheNumber(15);
+		System.out.println(num);
+		
+
+	}
+	public static int doubleTheNumber(int num) {
+		return num*2;
+	}
+
+}
