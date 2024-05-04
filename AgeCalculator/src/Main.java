caba3ef13b803b3b697828c4051a5a682fa1d647
sangeetagorle <sangeethagorle2@gmail.com>
2024-05-04 11:39:13 +0530

    ic

---
+++ AgeCalculator/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main
+
+{
+
+public static void categorizeAge(int age)
+{
+	if(age>0 && age<=12) {
+		System.out.println("Child"); 
+	}
+	else if(age>=13 && age<=19) {
+		System.out.println("Teen");
+	}
+	else if(age>=20 && age<=59) {
+		System.out.println("Adult");
+	}
+	else {
+		System.out.println("Senior");
+	}
+
+}
+
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	System.out.println("Enter your age");
+	int age=scan.nextInt();
+	categorizeAge(age);
+
+
+}
+}
