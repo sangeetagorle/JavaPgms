e70a0d27b3490ccbe44f220f46b0988f5dba0521
sangeetagorle <sangeethagorle2@gmail.com>
2024-05-04 14:45:31 +0530

    c

---
+++ StringJoiner/src/StringJoiner.java
@@ -0,0 +1,12 @@
+
+public class StringJoiner {
+	public static void main(String []args) {
+		String res=joinStrings("Hello, ", "World!");
+		System.out.println(res);
+		
+	}
+	public static String joinStrings(String str1, String str2)
+	{
+	return str1+str2;
+}
+}
